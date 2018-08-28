c9d707814abb856da00cfe6b536ea8068425b26a
lukas.holzer <lukas.holzer@dynatrace.com>
2018-08-28 11:07:43 +0200

    UX-8237 test(sketch-validator): add test for duplicate names

--- src/validate/rules/symbol-name-validation/symbol-name-validation.test.ts
+++ src/validate/rules/symbol-name-validation/symbol-name-validation.test.ts
@@ -1,5 +1,5 @@
 import { checkThemeInName, CONTAIN_THEME_NAME_ERROR, symbolNameValidation } from './symbol-name-validation';
-import { WrongSymbolNamingError } from '../../error/validation-error';
+import { WrongSymbolNamingError, DuplicatedSymbolError } from '../../error/validation-error';
 
 describe('Symbol Name Validation', () => {
 
@@ -42,5 +42,25 @@ describe('Symbol Name Validation', () => {
 
     const result = symbolNameValidation(fakeHomeworks, 0);
     expect(result).toBeTruthy();
+  });
+
+  it('should check for duplicate components', () => {
+    const fakeHomeworks = [
+      {
+        _class: 'symbolMaster',
+        do_objectID: 'C8BAFBE8-F0F0-4727-B952-7303F9CA3F33',
+        name: 'component-name\/light-bg\/state',
+      },
+      {
+        _class: 'symbolMaster',
+        do_objectID: 'C8BAFBE8-F0F0-4727-B952-7303F9CA3F33',
+        name: 'component-name\/light-bg\/state',
+      },
+    ] as any[];
+
+    const result = symbolNameValidation(fakeHomeworks, 0);
+    expect(result).toBeInstanceOf(Array);
+    expect(result).toHaveLength(1);
+    expect(result[0]).toBeInstanceOf(DuplicatedSymbolError);
   });
 });
