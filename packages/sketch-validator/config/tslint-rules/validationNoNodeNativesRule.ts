0148e2f42a8f05b83ddb6e5191f44866c3015b62
Lukas Holzer <lukas.holzer@dynatrace.com>
2018-12-03 17:08:40 +0100

    UX-8720 refactor(library): update tslint

--- packages/sketch-validator/config/tslint-rules/validationNoNodeNativesRule.ts
+++ packages/sketch-validator/config/tslint-rules/validationNoNodeNativesRule.ts
@@ -1,19 +1,17 @@
 import { IOptions, IRuleMetadata, RuleFailure, Rules, RuleWalker } from 'tslint';
 import * as ts from 'typescript';
+import chalk from 'chalk'
 
 const NODE_NATIVES = ['path', 'fs', 'os', 'buffer', 'crypto', 'util', 'child_process', 'perf_hooks'];
-const FAILURE_UTILS = `
-The {blue import ... from '@utils'} is a barrel file which exports node natives,
-like fs. Specify the import to a file where no node natives are used!`;
+const FAILURE_UTILS = chalk`
+The {blue import ... from '@sketchmine/node-helpers'} uses node natives, like fs.
+Please use the @sketchmine/helpers which are platform independent!`;
 
-const FAILURE_NODE_NATIVES = (m: string) => `
-The use of importing node natives like {blue ${m}} is forbidden in the validation except in the {grey run.ts} file,
-in case it has to work cross plattform.
+const FAILURE_NODE_NATIVES = (m: string) => chalk`
+The use of importing node natives like {blue ${m}} is forbidden in the validation,
+in case it has to work cross platform.
 {yellow This rules are going to be used in a 💎 .sketch plugin as well!}
 Provide all necessary data with the options in the config and gather them in the run file first.`;
-
-/** @see https://regex101.com/r/VvI102/1 */
-const FILES_REGEX = new RegExp(/\/src\/validate\/(?!(?:index|main|interfaces|run|rules\/file-name-validation)).+/gm);
 
 class ValidationNoNodeNativesRuleWalker extends RuleWalker {
 
@@ -23,7 +21,10 @@ class ValidationNoNodeNativesRuleWalker extends RuleWalker {
 
   visitImportDeclaration(node: ts.ImportDeclaration) {
     const moduleSpecifier = (node.moduleSpecifier as ts.StringLiteral).text;
-    if (moduleSpecifier.match(/\@utils$/gm)) {
+    if (
+      moduleSpecifier.match(/\@sketchmine\/node-helpers$/gm) ||
+      moduleSpecifier.match(/\@sketchmine\/sketch-file-builder$/gm)
+    ) {
       this.addFailureAt(node.getStart(), node.getWidth(), `${FAILURE_UTILS}\n${node.getText()}`);
     }
     if (NODE_NATIVES.includes(moduleSpecifier)) {
@@ -53,10 +54,7 @@ export class Rule extends Rules.AbstractRule {
   };
 
   apply(sourceFile: ts.SourceFile): RuleFailure[] {
-    if (
-      sourceFile.fileName.match(FILES_REGEX) &&
-      !sourceFile.fileName.endsWith('.test.ts')
-    ) {
+    if (!sourceFile.fileName.endsWith('.test.ts')) {
       return this.applyWithWalker(new ValidationNoNodeNativesRuleWalker(sourceFile, this.getOptions()));
     }
     return [];
