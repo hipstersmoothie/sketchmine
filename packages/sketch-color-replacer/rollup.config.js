460e5837c58284136ce6af2cabe3b6a9329a0c4a
Lukas Holzer <lukas.holzer@dynatrace.com>
2018-12-06 13:55:00 +0100

    UX-8720 docs(sketch-color-replacer): add documentation for color replacer

--- packages/sketch-color-replacer/rollup.config.js
+++ packages/sketch-color-replacer/rollup.config.js
@@ -1,11 +1,24 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { banner } from '../../config/banner';
 import json from 'rollup-plugin-json'
 import pkg from './package.json';
 
 const NODE_NATIVES = ['path', 'fs', 'os', 'buffer', 'crypto', 'util', 'child_process', 'perf_hooks'];
 const DEPENDENCIES = Object.keys(pkg.dependencies);
+
+const external = [
+  ...NODE_NATIVES,
+  ...DEPENDENCIES,
+];
+
+const plugins = [
+  json(),
+  typescript({ tsconfig: './tsconfig.json', useTsconfigDeclarationDir: true }),
+  resolve(), // so Rollup can find `ms`
+  commonjs(), // so Rollup can convert `ms` to an ES module
+];
 
 export default [
   {
@@ -16,17 +29,10 @@ export default [
         file: pkg.main,
         format: 'cjs',
         sourcemap: true,
+        banner: `#!/usr/bin/env node\n\n${banner(pkg)}`,
       },
     ],
-    external: [
-      ...NODE_NATIVES,
-      ...DEPENDENCIES,
-    ],
-    plugins: [
-      json(),
-      typescript({ tsconfig: './tsconfig.json', useTsconfigDeclarationDir: true }),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-    ]
+    external,
+    plugins,
   },
 ]
