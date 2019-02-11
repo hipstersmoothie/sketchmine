699689e72a01b2f5167b69ce0c10dd24b72a90b1
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-02-11 12:41:40 +0100

    UX-9064 chore(sketch-validation-interface): highlight quoted text

--- packages/sketch-validation-interface/src/app/components/result-message/result-message.component.ts
+++ packages/sketch-validation-interface/src/app/components/result-message/result-message.component.ts
@@ -5,17 +5,21 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 const HEX_REGEX = /<(#[0-9a-fA-F]{3,6})>/igm;
 
 // tslint:disable:max-line-length
-/**
- * @see https://regexr.com/3amq5
- */
+/** @see https://regexr.com/3amq5 */
 const LINK_REGEX = /((?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
 // tslint:enable:max-line-length
+
+/** @see https://regex101.com/r/UwAkZe/1 */
+const QUOTES_REGEX = /(\".+?\")/gm;
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'result-message',
   template: '<p [innerHTML]="sanitizedMsg"></p>',
-  styles: ['.material-icons { vertical-align: -6px }'],
+  styles: [
+    '.material-icons { vertical-align: -6px }',
+    '.italic { font-style: italic; color: #898989; }',
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResultMessageComponent implements OnInit {
@@ -26,12 +30,17 @@ export class ResultMessageComponent implements OnInit {
   ngOnInit() {
     let msg = this.message.replace(HEX_REGEX, wrapHexInIcon);
     msg = msg.replace(LINK_REGEX, wrapLinkInAnchor);
+    msg = msg.replace(QUOTES_REGEX, wrapQuotesInItalic);
     this.sanitizedMsg = this.sanitizer.bypassSecurityTrustHtml(msg);
   }
 }
 
 function wrapHexInIcon(match: string, p1: string): string {
   return `<i class="material-icons" style="color: ${p1}">color_lens</i> ${p1}`;
+}
+
+function wrapQuotesInItalic(match: string): string {
+  return `<i class="italic">${match}</i>`;
 }
 
 function wrapLinkInAnchor(match: string): string {
