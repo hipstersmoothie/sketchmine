a5fa45bf596abeabf0901571b0971cc1f737beb9
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-02-14 12:43:47 +0100

    chore(code-analyzer): parse call expressions and decorators

--- packages/code-analyzer/src/utils/get-symbol-name.ts
+++ packages/code-analyzer/src/utils/get-symbol-name.ts
@@ -18,29 +18,44 @@ export function getSymbolName(node: any): string {
       return getSymbolName(node.declarationList);
     case ts.SyntaxKind.VariableDeclarationList:
       return node.declarations.map(declaration => getSymbolName(declaration)).join(', ');
+    case ts.SyntaxKind.ObjectLiteralExpression:
+    case ts.SyntaxKind.ArrayLiteralExpression:
+    case ts.SyntaxKind.ConstructorType:
+      // Array or Object Literal expressions can't have a name!
+      return;
+
   }
 
-  /** node is a StringLiteral */
+  // node is a StringLiteral
   if (node.literal && node.literal.kind === ts.SyntaxKind.StringLiteral) {
     return node.literal.text;
   }
-  /** node is a StringLiteral */
+  // node is a StringLiteral
   if (node.kind && node.kind === ts.SyntaxKind.StringLiteral) {
     return node.text;
   }
-  /** node is a Identifier */
+  // node is a Identifier
   if (node.kind && node.kind === ts.SyntaxKind.Identifier) {
     return node.text;
   }
-  if (node.name && node.name.kind === ts.SyntaxKind.Identifier) {
+
+  if (
+    node.name && node.name.kind === ts.SyntaxKind.Identifier ||
+    node.name && node.name.kind === ts.SyntaxKind.StringLiteral
+  ) {
     return node.name.text;
   }
-  /** node is a TypeReference */
+  // node is a TypeReference
   if (node.typeName && node.kind === ts.SyntaxKind.TypeReference) {
     return node.typeName.text;
   }
 
-  log.debug(
+  // call expression
+  if (node.expression.kind === ts.SyntaxKind.Identifier) {
+    return node.expression.text;
+  }
+
+  log.warning(
     chalk`Unsupported Syntax kind {bgBlue  <${ts.SyntaxKind[node.kind]}> } ` +
     chalk`{grey – function getSymbolName(node)}`,
   );
